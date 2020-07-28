5c2dc010c080856b36fb366b394403ef7a9c503e
Igor <igorsilvasf4@gmail.com>
2020-07-28 11:39:12 -0300

    enter

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,40 @@
 |
 */
 
+use App\User;
+
 Route::get('/', function () {
     $helloWorld = 'Hello Word!';
     return view('welcome', ['helloWorld'=> $helloWorld]);
+});
+
+Route::get('/model', function(){
+    $products = \App\Product::all();
+
+   // $user = new \App\User();
+    $user = \App\User::find(1);
+    $user->name = 'Usuario Teste Editado';
+    $user->save();
+
+   // \App\User::all
+   // return  \App\User::find('3')
+   // \App\User::where('name', 'Verona McDermott I')->first()
+   // \App\User::paginate(10)
+
+   //Mass Assignment
+
+//    $user = \App\User::create([
+//         'name' => 'Nanderson Castro',
+//         'email' => 'email100@email.com',
+//         'password' => bcrypt('1234566')
+//    ]);
+
+    //Mass Update
+
+    $user = \App\User::find(42);
+    $user->update([
+        'name' => 'Atualizando com Mass Update'
+    ]);
+dd($user);
+    return \App\User::all();
 });
