4675f1e385d4f84457ec4dc080d53ad9d504068e
Igor <igorsilvasf4@gmail.com>
2020-07-28 23:05:06 -0300

    enter

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+use App\Store;
 use App\User;
 
 Route::get('/', function () {
@@ -41,10 +42,73 @@ Route::get('/model', function(){
 
     //Mass Update
 
-    $user = \App\User::find(42);
-    $user->update([
-        'name' => 'Atualizando com Mass Update'
-    ]);
-dd($user);
+    // $user = \App\User::find(42);
+    // $user->update([
+    //     'name' => 'Atualizando com Mass Update'
+    // ]);
+
+    // $user = \App\User::find(4);
+
+    // dd( $user->store()->count());
+
+    // $loja = \App\Store::find(1);
+    // return $loja->products()->where('id',1)->get();
+
+    // $categoria = \App\Category::find(1);
+    // $categoria->products;
+
+    // $user = \App\User::find(10);
+    // $store = $user->store()->create([
+    //     'name' => 'Loja Teste',
+    //     'description' => 'Loja teste produtos informatica',
+    //     'mobile_phone' => 'xx-xxxx-xxxx',
+    //     'phone' => 'xx-xxx-xxxx',
+    //     'slug' => 'loja-teste'
+    // ]);
+
+//    $store = \App\Store::find(42);
+//    $product = $store->products()->create([
+//     'name' => 'Notebook Dell',
+//     'description' => 'Core I5 10gb',
+//     'body' => 'Qualquer coisa',
+//     'price' => 2999.90,
+//     'slug' => 'notebook-dell',
+//    ]);
+
+//     dd($product);
+
+
+
+
+    // \App\Category::create([
+    //     'name' => 'Games',
+    //     'description' => null,
+    //     'slug' => 'games'
+    // ]);
+
+    // \App\Category::create([
+    //     'name' => 'Notebook',
+    //     'description' => null,
+    //     'slug' => 'notebook'
+    // ]);
+
+    // return \App\Category::all();
+
+    // $product = \App\Product::find(49);
+    // dd($product->categories()->sync([2]));
+
     return \App\User::all();
+});
+
+
+Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
+
+    Route::prefix('stores')->name('stores.')->group(function(){
+        Route::get('/','StoreController@index')->name('index');   
+        Route::get('/create','StoreController@create')->name('create'); 
+        Route::post('/store','StoreController@store')->name('store'); 
+        Route::get('/{store}/edit','StoreController@edit')->name('edit'); 
+        Route::post('/update/{store}','StoreController@update')->name('update'); 
+        Route::get('/destroy/{store}','StoreController@destroy')->name('destroy');
+    });
 });
